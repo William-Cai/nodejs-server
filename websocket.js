000e45c843bdf7b6c3a5334f8580bba5e0fd6923
githubwinnie <caiwlwinnie@gmail.com>
2017-07-06 17:21:45 +0800

    后端node.js使用模块ws，客户端使用WebSocket测试

---
+++ websocket.js
@@ -0,0 +1,57 @@
+/**
+ * @Title
+ * @Description
+ * @Author Winnie.Cai
+ * @CreteData 2017/7/6.
+ */
+var http = require("http");
+
+var url = require("url");
+
+var fs = require("fs");
+
+var server = http.createServer(function (request, response) {
+
+    var requestUrl = request.url;
+
+    if (/\w+\.(html|js)/.test(requestUrl)) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        fs.readFile("." + requestUrl, function (err, data) {
+            if (err) {
+                console.log(err);
+                // HTTP 状态码: 404 : NOT FOUND
+                // Content Type: text/plain
+                response.writeHead(404, {'Content-Type': 'text/html'});
+            } else {
+                // HTTP 状态码: 200 : OK
+                // Content Type: text/plain
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                // 响应文件内容
+                response.write(data.toString());
+            }
+            //  发送响应数据
+            response.end();
+        });
+    } else {
+        response.writeHead(404);
+    }
+});
+
+server.listen(8181);
+
+var WebSocketServer = require('ws').Server
+    , wss = new WebSocketServer({port: 8080});
+
+wss.on('connection', function (ws) {
+    ws.on('message', function (message) {
+        console.log('received: %s', message);
+        //遍历所有客户端，发送全局消息
+        wss.clients.forEach(function each(client) {
+            client.send(JSON.stringify("某某某 Say:" + message));
+        });
+    });
+    ws.on('close', function (close) {
+        console.log('剩余数量：' + wss.clients.size);
+    });
+    ws.send(JSON.stringify('Connection Success. Count:' + wss.clients.size));
+});
